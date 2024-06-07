8aa7c9bb7644c7dda1d25c07ae4ea6d1424296ba
Makoto Katsumata <mako10k@mk10.org>
2024-06-07 22:53:54 +0900

    major rafactoring

--- src/Data/Digit/Generic.hs
+++ src/Data/Digit/Generic.hs
@@ -5,11 +5,16 @@
 
 module Data.Digit.Generic
   ( GenDigit (),
-    pattern LenientGenDigit,
+    pattern GenDigit,
+    pattern GenDigitMod,
+    pattern GenDigitIx,
   )
 where
 
 import Data.Bifunctor (Bifunctor (first, second))
+import Data.Char (isDigit, isLower, isUpper)
+import Data.Coerce (coerce)
+import Data.Data (Proxy (Proxy))
 import Data.Digit
   ( Digit
       ( digitMax,
@@ -20,89 +25,99 @@ import Data.Digit
         toDigits
       ),
   )
-import Data.Digit.Generic.GDIx
+import Data.Digit.Generic.GDIx (GDIx (GDIx))
 import Data.Function (on)
-import Data.List (singleton)
-import GHC.TypeNats (KnownNat)
+import GHC.TypeNats (KnownNat, natVal)
 
 -- | Represents a generic digit type with negative bound and positive bound.
 data GenDigit neg pos int where
   -- | A digit in the range from @-neg@ to @pos@. (Do not use this constructor directly, use 'GenDigit' instead.)
-  StrictGenDigit :: (KnownNat neg, KnownNat pos, Integral int) => StrictGDIx neg pos int -> GenDigit neg pos int
+  UnsafeGenDigit :: int -> GenDigit neg pos int
 
--- | A pattern synonym for 'GenDigit'.
-pattern LenientGenDigit :: (KnownNat neg, KnownNat pos, Integral int) => LGDIx neg pos int -> GenDigit neg pos int
-pattern LenientGenDigit ix <- StrictGenDigit (convertGDIx -> ix)
+pattern GenDigit :: forall neg pos int. (KnownNat neg, KnownNat pos, Integral int) => int -> GenDigit neg pos int
+pattern GenDigit int <- UnsafeGenDigit int
   where
-    LenientGenDigit = StrictGenDigit . convertGDIx
-
-{-# COMPLETE LenientGenDigit #-}
-
-pattern GenDigit :: (KnownNat neg, KnownNat pos, Integral int) => int -> GenDigit neg pos int
-pattern GenDigit ix = StrictGenDigit (StrictGDIx ix)
+    GenDigit int | neg <= int && int <= pos = UnsafeGenDigit int
+      where
+        neg = -fromIntegral (natVal (Proxy @neg))
+        pos = fromIntegral (natVal (Proxy @pos))
+    GenDigit _ = error "GenDigit: index out of bounds"
 
 {-# COMPLETE GenDigit #-}
 
+pattern GenDigitMod :: forall neg pos int. (KnownNat neg, KnownNat pos, Integral int) => int -> GenDigit neg pos int
+pattern GenDigitMod int <- UnsafeGenDigit int
+  where
+    GenDigitMod int = UnsafeGenDigit ((int - neg) `mod` (pos - neg + 1) + neg)
+      where
+        neg = -fromIntegral (natVal (Proxy @neg))
+        pos = fromIntegral (natVal (Proxy @pos))
+
+{-# COMPLETE GenDigitMod #-}
+
+pattern GenDigitIx :: forall neg pos int. (KnownNat neg, KnownNat pos, Integral int) => GDIx neg pos int -> GenDigit neg pos int
+pattern GenDigitIx ix <- GenDigit (coerce -> ix)
+  where
+    GenDigitIx = GenDigit . coerce
+
+{-# COMPLETE GenDigitIx #-}
+
 -- | Check if an integer is in the range of a 'GenDigit'.
 instance (KnownNat neg, KnownNat pos, Integral int) => Show (GenDigit neg pos int) where
-  show (GenDigit int) = maybe (showParen True (shows (toInteger int)) "") singleton $ toCharGDIx @LGDIx @neg @pos @int (fromIntegral int)
+  show (GenDigit int)
+    | 0 <= int && int <= 9 = [toEnum (fromIntegral int + fromEnum '0')]
+    | 10 <= int && int <= 35 = [toEnum (fromIntegral int - 10 + fromEnum 'A')]
+    | -26 <= int && int < -1 = [toEnum (-fromIntegral int - 1 + fromEnum 'a')]
+    | otherwise = "(" ++ show (toInteger int) ++ ")"
   showList [] = showString "0"
   showList xs = showString (concatMap show (reverse xs))
 
 instance (KnownNat neg, KnownNat pos, Integral int) => Read (GenDigit neg pos int) where
   readsPrec _ [] = []
-  readsPrec _ ('0' : xs) = [(0, xs)]
-  readsPrec _ ((fromCharGDIx -> Just ix) : xs) = [(StrictGenDigit ix, xs)]
-  readsPrec d xs = first StrictGenDigit <$> readsPrec d xs
+  readsPrec _ (x : xs)
+    | isDigit x = [(GenDigit (fromIntegral (fromEnum x - fromEnum '0')), xs)]
+    | isUpper x = [(GenDigit (fromIntegral (fromEnum x - fromEnum 'A' + 10)), xs)]
+    | isLower x = [(GenDigit (fromIntegral (-fromEnum x + fromEnum 'a' - 1)), xs)]
+  readsPrec d xs = first (GenDigit . fromInteger) <$> readsPrec d xs
 
 instance (KnownNat neg, KnownNat pos, Integral int) => Digit (GenDigit neg pos int) where
   fromDigit (GenDigit int) = fromIntegral int
-  toDigit int = GenDigit $ (fromIntegral int - fromGDIx minGDIx') `mod` fromGDIx sizeGDIx' + fromGDIx minGDIx'
-    where
-      minGDIx' :: StrictGDIx neg pos int
-      minGDIx' = minGDIx
-      sizeGDIx' :: StrictGDIx neg pos int
-      sizeGDIx' = sizeGDIx
-  digitMin = StrictGenDigit minGDIx
-  digitMax = StrictGenDigit maxGDIx
+  toDigit int = GenDigit (fromIntegral int)
+  digitMin = GenDigit (-fromIntegral (natVal (Proxy @neg)))
+  digitMax = GenDigit (fromIntegral (natVal (Proxy @pos)))
 
-instance (KnownNat neg, KnownNat pos, Integral int) => Num (GenDigit neg pos int) where
-  LenientGenDigit x + LenientGenDigit y = LenientGenDigit $ x + y
-  LenientGenDigit x * LenientGenDigit y = LenientGenDigit $ x * y
-  LenientGenDigit x - LenientGenDigit y = LenientGenDigit $ x - y
-  abs (LenientGenDigit x) = LenientGenDigit $ abs x
-  signum (LenientGenDigit x) = LenientGenDigit $ signum x
-  fromInteger = LenientGenDigit . fromInteger
-
-onWithCarry :: forall neg pos int. (KnownNat neg, KnownNat pos, Integral int) => (LGDIx neg pos int -> LGDIx neg pos int -> LGDIx neg pos int) -> LGDIx neg pos int -> LGDIx neg pos int -> (LGDIx neg pos int, LGDIx neg pos int)
-onWithCarry (|?|) x y = second (minGDIx +) ((x |?| y - minGDIx) `divMod` sizeGDIx)
+onWithCarry :: forall neg pos int. (KnownNat neg, KnownNat pos, Integral int) => (GDIx neg pos int -> GDIx neg pos int -> GDIx neg pos int) -> GDIx neg pos int -> GDIx neg pos int -> (GDIx neg pos int, GDIx neg pos int)
+onWithCarry (|?|) x y = second (neg +) ((x |?| y - neg) `divMod` (pos - neg + 1))
+  where
+    neg = -fromIntegral (natVal (Proxy @neg))
+    pos = fromIntegral (natVal (Proxy @pos))
 
 instance (KnownNat pos, KnownNat neg, Integral int) => Num [GenDigit pos neg int] where
-  (+) = f 0
+  (+) = go 0
     where
-      f c [] []
-        | c == 0 = 0
-        | otherwise = [LenientGenDigit c]
-      f c [] ys = f c [0] ys
-      f c xs [] = f c xs [0]
-      f c (LenientGenDigit x : xs) (LenientGenDigit y : ys) = LenientGenDigit z2 : f (c1 + c2) xs ys
+      go cix [] []
+        | cix == 0 = 0
+        | otherwise = [GenDigitIx cix]
+      go cix [] ys = go cix [GenDigitIx 0] ys
+      go cix xs [] = go cix xs [GenDigitIx 0]
+      go cix (GenDigitIx x : xs) (GenDigitIx y : ys) = GenDigitIx z2 : go (cix1 + cix2) xs ys
         where
-          (c1, z1) = onWithCarry (+) x y
-          (c2, z2) = onWithCarry (+) z1 c
+          (cix1, z1) = onWithCarry (+) x y
+          (cix2, z2) = onWithCarry (+) z1 cix
 
   xs * ys = fromInteger $ on (*) fromDigits xs ys
 
-  (-) = f 0
+  (-) = go 0
     where
-      f c [] []
-        | c == 0 = 0
-        | otherwise = [LenientGenDigit c]
-      f c [] ys = f c [0] ys
-      f c xs [] = f c xs [0]
-      f c (LenientGenDigit x : xs) (LenientGenDigit y : ys) = LenientGenDigit z2 : f (c1 + c2) xs ys
+      go cix [] []
+        | cix == 0 = 0
+        | otherwise = [GenDigitIx cix]
+      go cix [] ys = go cix [GenDigitIx 0] ys
+      go cix xs [] = go cix xs [GenDigitIx 0]
+      go cix (GenDigitIx x : xs) (GenDigitIx y : ys) = GenDigitIx z2 : go (cix1 + cix2) xs ys
         where
-          (c1, z1) = onWithCarry (-) x y
-          (c2, z2) = onWithCarry (-) z1 c
+          (cix1, z1) = onWithCarry (-) x y
+          (cix2, z2) = onWithCarry (-) z1 cix
   abs = fromInteger . abs . fromDigits
   signum = fromInteger . signum . fromDigits
   fromInteger = toDigits
--- src/Data/Digit/Generic/GDIx.hs
+++ src/Data/Digit/Generic/GDIx.hs
@@ -1,108 +1,22 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RoleAnnotations #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ViewPatterns #-}
 
 -- |
 -- Module      : Data.Digit.Generic.GDIx
 -- Description : Module for generic digit operations using Index type
 module Data.Digit.Generic.GDIx
   ( -- * GDIx type
-    LGDIx (LGDIx),
-    StrictGDIx (),
-    pattern StrictGDIx,
-    GDIx (..),
+    GDIx (GDIx),
   )
 where
 
-import Control.Applicative (Alternative (empty))
-import Control.Monad (guard)
-import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
-import Data.Coerce (coerce)
-import Data.Data (Proxy (Proxy))
-import Data.Function (on)
-import GHC.TypeNats (KnownNat, Nat, natVal)
+import GHC.TypeNats (Nat)
 
-class (KnownNat neg, KnownNat pos, Integral int) => GDIx ix neg pos int where
-  fromGDIx :: ix neg pos int -> int
-  toGDIx :: int -> ix neg pos int
-  minGDIx :: ix neg pos int
-  minGDIx = toGDIx (-fromIntegral (natVal (Proxy @neg)))
-  maxGDIx :: ix neg pos int
-  maxGDIx = toGDIx (fromIntegral (natVal (Proxy @pos)))
-  sizeGDIx :: ix neg pos int
-  sizeGDIx = toGDIx (fromIntegral (natVal (Proxy @pos)) - fromIntegral (natVal (Proxy @neg)) + 1)
-  convertGDIx :: (GDIx ix' neg pos int) => ix neg pos int -> ix' neg pos int
-  convertGDIx = toGDIx . fromGDIx
-  idSafeGDIx :: (Alternative t) => ix neg pos int -> t (ix neg pos int)
-  idSafeGDIx ix = ix <$ guard (minGDIx <?= ix && ix <?= maxGDIx)
-    where
-      (<?=) = on (<=) fromGDIx
-  toSafeGDIx :: (Alternative t) => int -> t (ix neg pos int)
-  toSafeGDIx int = toGDIx int <$ guard (fromGDIx' minGDIx <= int && int <= fromGDIx' maxGDIx)
-    where
-      fromGDIx' = fromGDIx :: ix neg pos int -> int
-  toCharGDIx :: (Alternative t) => ix neg pos int -> t Char
-  toCharGDIx = toCharGDIx' . toGDIxSafe' . fromGDIx
-    where
-      toCharGDIx' (Just (LGDIx int))
-        | 0 <= int && int <= 9 = pure $ toEnum $ 48 + fromIntegral int
-        | 10 <= int && int <= 36 = pure $ toEnum $ 55 + fromIntegral int
-        | -26 <= int && int <= -1 = pure $ toEnum $ 96 - fromIntegral int
-      toCharGDIx' _ = empty
-      toGDIxSafe' :: (Alternative t) => int -> t (LGDIx neg pos int)
-      toGDIxSafe' = toSafeGDIx
-  fromCharGDIx :: (Alternative t) => Char -> t (ix neg pos int)
-  fromCharGDIx ch
-    | isDigit ch = toSafeGDIx $ fromIntegral $ fromEnum ch - 48
-    | isAsciiUpper ch = toSafeGDIx $ fromIntegral $ fromEnum ch - 55
-    | isAsciiLower ch = toSafeGDIx $ fromIntegral $ 96 - fromEnum ch
-    | otherwise = empty
-
-type role LGDIx phantom phantom representational
+type role GDIx phantom phantom representational
 
 -- | A generic type representing a digit index.
-newtype LGDIx (neg :: Nat) (pos :: Nat) int
+newtype GDIx (neg :: Nat) (pos :: Nat) int
   = -- | A digit index with a value of type @int@.
-    LGDIx int
+    GDIx int
   deriving (Eq, Ord, Num, Real, Enum, Integral)
-
-instance (KnownNat neg, KnownNat pos, Integral int) => GDIx LGDIx neg pos int where
-  fromGDIx = coerce
-  toGDIx = coerce
-
-instance (KnownNat neg, KnownNat pos, Integral int) => Read (LGDIx neg pos int) where
-  readsPrec d s = [(fromInteger int, t) | (int, t) <- readsPrec d s]
-
-instance (KnownNat neg, KnownNat pos, Integral int) => Show (LGDIx neg pos int) where
-  showsPrec d ix = showsPrec d (toInteger ix)
-
-instance (KnownNat neg, KnownNat pos, Integral int) => Read (StrictGDIx neg pos int) where
-  readsPrec d s = [(ix, t) | (int, t) <- readsPrec d s, ix <- toSafeGDIx (fromInteger int)]
-
-instance (KnownNat neg, KnownNat pos, Integral int) => Show (StrictGDIx neg pos int) where
-  showsPrec d ix = showsPrec d (toInteger ix)
-
-type role StrictGDIx phantom phantom representational
-
-newtype StrictGDIx (neg :: Nat) (pos :: Nat) int
-  = UnsafeStrictGDIx int
-  deriving (Eq, Ord, Num, Real, Enum, Integral)
-
-pattern StrictGDIx :: forall neg pos int. (KnownNat neg, KnownNat pos, Integral int) => int -> StrictGDIx neg pos int
-pattern StrictGDIx int <- (coerce -> int)
-  where
-    StrictGDIx = toGDIx
-
-{-# COMPLETE StrictGDIx #-}
-
-instance (KnownNat neg, KnownNat pos, Integral int) => GDIx StrictGDIx neg pos int where
-  fromGDIx = coerce
-  toGDIx int = case toSafeGDIx int of
-    Just ix -> ix
-    Nothing -> error "StrictGDIx: out of range"
-
-instance (KnownNat neg, KnownNat pos, Integral int) => Bounded (StrictGDIx neg pos int) where
-  minBound = minGDIx
-  maxBound = maxGDIx
